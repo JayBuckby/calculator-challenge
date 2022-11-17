48bdd9279d739ab11e24b432f510e218a5210032
Jay <JJ.Buckby@hotmail.co.uk>
2022-11-17 16:55:08 +0000

    tidied html classes

--- script/script.js
+++ script/script.js
@@ -1,3 +1,4 @@
 const numbers = document.querySelectorAll("numbers");
 const operations = document.querySelector("operations");
-
+const equals = document.querySelector("equals");
+const positiveNegative = document.querySelector("positiveNegative");
