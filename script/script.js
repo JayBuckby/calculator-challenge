d66ce082ce57aab3d6629be2569e570b02689aa8
Jay <JJ.Buckby@hotmail.co.uk>
2022-11-21 16:57:48 +0000

    Created display and event listener

--- script/script.js
+++ script/script.js
@@ -1,4 +1,22 @@
-const numbers = document.querySelectorAll("numbers");
-const operations = document.querySelector("operations");
-const equals = document.querySelector("equals");
-const positiveNegative = document.querySelector("positiveNegative");
+const numbers = Array.from(document.querySelectorAll(".numbers"));
+const operators = Array.from(document.querySelectorAll(".operators"));
+const equals = document.querySelector(".equals");
+const clear = document.querySelector(".delete");
+const positiveNegative = document.querySelector(".postiveNegative");
+const display = document.querySelector(".display");
+
+numbers.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.innerHTML += button.innerHTML;
+  });
+});
+
+operators.forEach((button) => {
+  button.addEventListener("click", () => {
+    display.innerHTML += button.innerHTML;
+  });
+});
+
+clear.addEventListener("click", () => {
+  return (display.innerHTML = "");
+});
