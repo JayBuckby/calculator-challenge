ace7d69d8783c3e544e39de9dbeb0bd72493c60b
Jay <JJ.Buckby@hotmail.co.uk>
2022-11-22 17:20:00 +0000

    Started math script for calc

--- script/script.js
+++ script/script.js
@@ -5,18 +5,54 @@ const clear = document.querySelector(".delete");
 const positiveNegative = document.querySelector(".postiveNegative");
 const display = document.querySelector(".display");
 
+let num1 = "";
+let num2 = "";
+let operator = "";
+
 numbers.forEach((button) => {
   button.addEventListener("click", () => {
     display.innerHTML += button.innerHTML;
   });
+  if (operator == "") {
+    num1 += button.innerHTML;
+  } else {
+    num2 += button.innerHTML;
+  }
 });
 
 operators.forEach((button) => {
   button.addEventListener("click", () => {
-    display.innerHTML += button.innerHTML;
+    if (num1 != "") {
+      display.innerHTML += button.innerHTML;
+    }
   });
 });
 
 clear.addEventListener("click", () => {
   return (display.innerHTML = "");
 });
+
+const mathsEquation = (num1, num2, operator) => {
+  let newnum1 = Number(num1);
+  console.log(num1, newnum1);
+  let newnum2 = Number(num2);
+  console.log(num2, newnum2);
+
+  if (operator === "+") {
+    return newnum1 + newnum2;
+  } else if (operator === "-") {
+    return newnum1 - newnum2;
+  } else if (operator === "÷") {
+    return newnum1 / newnum2;
+  } else if (operator === "x") {
+    return newnum1 * newnum2;
+  }
+
+  console.log(operator);
+};
+
+const handleSum = () => {
+  console.log(mathsEquation(num1, num2, operator));
+};
+
+equals.addEventListener("click", handleSum);
